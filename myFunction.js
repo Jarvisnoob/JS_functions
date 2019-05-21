a8da4ff4b98581729161d79b35172c82ef4c71bd
Jarvisnoob <adamsmith9707@gmail.com>
2019-05-21 21:01:13 +0800

    for test

--- myFunction.js
+++ myFunction.js
@@ -1,4 +1,5 @@
 function getChildElementNodes(parentNode){
+    console.log("js 引用成功");
     var raw_childs = parentNode.childNodes;
     var elementChilds = [];
     for(var count = 0;count < raw_childs.length;count++){
