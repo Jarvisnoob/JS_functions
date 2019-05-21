01ae36fd7841c20546d97e51ba53bd63bf9a2329
Jarvisnoob <adamsmith9707@gmail.com>
2019-05-21 20:49:32 +0800

    First function: getChildElementNodes

---
+++ myFunction.js
@@ -0,0 +1,10 @@
+function getChildElementNodes(parentNode){
+    var raw_childs = parentNode.childNodes;
+    var elementChilds = [];
+    for(var count = 0;count < raw_childs.length;count++){
+        if(raw_childs[count] && raw_childs[count].nodeType == 1){
+            elementChilds.push(raw_childs[count]);
+        }
+    }
+    return elementChilds;
+}
