29179591be38177d1ceb98dc9d10d535d9190392
Jarvisnoob <adamsmith9707@gmail.com>
2019-05-22 20:13:36 +0800

    add new functions

--- myFunction.js
+++ myFunction.js
@@ -1,11 +1,39 @@
-function getChildElementNodes(parentNode){
-    console.log("js 引用成功");
-    var raw_childs = parentNode.childNodes;
-    var elementChilds = [];
-    for(var count = 0;count < raw_childs.length;count++){
-        if(raw_childs[count] && raw_childs[count].nodeType == 1){
-            elementChilds.push(raw_childs[count]);
+function getEleId(id){
+    return document.getElementById(id);
+}
+
+function getChildEleNodes(parentNode){
+    console.log("getChildEleNodes 引用成功");
+    var raw_children = parentNode.childNodes;
+    var elementChildren = [];
+    for(var count = 0;count < raw_children.length;count++){
+        if(raw_children[count] && raw_children[count].nodeType == 1){
+            elementChildren.push(raw_children[count]);
         }
     }
-    return elementChilds;
+    return elementChildren;
+}
+
+function get1stChildEle(parentNode){
+    var isEle = false;
+    var childNodes = parentNode.childNodes;
+    var count = 0;
+    while(!isEle && count < childNodes.length){
+        if(childNodes[count] && childNodes[count].nodeType==1){
+            isEle = true;
+        }else{
+            count++;
+        }
+    }
+    return childNodes[count];
+}
+
+function eleFilter(nodesArr){
+    var elesArr = new Array();
+    for (var count = 0; count < nodesArr.length; count++){
+        if(nodesArr[count] && nodesArr[count].nodeType==1){
+            elesArr.push(nodesArr[count]);
+        }
+    }
+    return elesArr;
 }
